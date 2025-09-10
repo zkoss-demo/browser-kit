2852bc948d9f151c56defcb7c252c5e2f471c83e
hawkchen <hawkchen@potix.com>
2025-09-10 14:17:22 +0800

    refactor to smaller methods

--- src/main/java/org/zkoss/zkforge/geolocation/GeolocationHelper.java
+++ src/main/java/org/zkoss/zkforge/geolocation/GeolocationHelper.java
@@ -13,27 +13,32 @@ import java.util.function.Consumer;
  * Based on https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API
  */
 public class GeolocationHelper {
-    private static final String GEOLOCATION_HELPER_KEY = "browserkit.geolocation.helper";
-    private static final String GEOLOCATION_HELPER_JS_PATH = "~./js/GeolocationHelper.js";
-    public static final String ON_GET_LOCATION = "onGetLocation";
+    protected static final String GEOLOCATION_HELPER_KEY = "browserkit.geolocationhelper";
+    protected static String GEOLOCATION_HELPER_JS_PATH = "~./js/GeolocationHelper.js";
+    public static final String EVENT_NAME = "onGetLocation";
+    protected Script geolocationHelperScript;
 
-    private final Consumer<GeoLocationPosition> positionCallback;
-    private final Consumer<GeolocationPositionError> errorCallback;
+    protected Consumer<GeoLocationPosition> positionCallback;
+    protected Consumer<GeolocationPositionError> errorCallback;
+    protected GeolocationResultListener listener;
+    protected Desktop desktop;
 
     public GeolocationHelper(Consumer<GeoLocationPosition> positionCallback,
                            Consumer<GeolocationPositionError> errorCallback) {
         ensureExecutionAvailable();
         ensureDesktopScopeSingleton();
+        setCallbacks(positionCallback, errorCallback);
+        addPageListener();
+        addHelperJavaScript(GEOLOCATION_HELPER_JS_PATH);
+    }
 
+    protected void setCallbacks(Consumer<GeoLocationPosition> positionCallback, Consumer<GeolocationPositionError> errorCallback) {
         this.positionCallback =  Objects.requireNonNull(positionCallback, "positionCallback cannot be null");
         this.errorCallback = Objects.requireNonNull(errorCallback, "errorCallback cannot be null");
-
-        initResultHandling();
-        initHelperJavaScript(GEOLOCATION_HELPER_JS_PATH);
     }
 
     protected void ensureDesktopScopeSingleton() {
-        Desktop desktop = Executions.getCurrent().getDesktop();
+        desktop = Executions.getCurrent().getDesktop();
         // Enforce singleton constraint
         if (desktop.getAttribute(GEOLOCATION_HELPER_KEY) != null) {
             throw new IllegalStateException(
@@ -43,26 +48,38 @@ public class GeolocationHelper {
         desktop.setAttribute(GEOLOCATION_HELPER_KEY, this);
     }
 
-    protected static void initHelperJavaScript(String jsPath) {
-        Script geolocationHelperScript = new Script();
+    protected void addHelperJavaScript(String jsPath) {
+        geolocationHelperScript = new Script();
         geolocationHelperScript.setSrc(jsPath);
-        geolocationHelperScript.setPage(Executions.getCurrent().getDesktop().getFirstPage());
+        geolocationHelperScript.setPage(desktop.getFirstPage());
     }
 
-    private void initResultHandling() {
+    protected void removeHelperJavaScript() {
+        geolocationHelperScript.detach();
+    }
 
-        GeolocationResultListener listener = new GeolocationResultListener();
+    protected void addPageListener() {
+        listener = new GeolocationResultListener();
         listener.setCallbacks(positionCallback, errorCallback);
-        Executions.getCurrent().getDesktop().getFirstPage().addEventListener(ON_GET_LOCATION, listener);
+        desktop.getFirstPage().addEventListener(EVENT_NAME, listener);
+    }
+
+    protected void removePageListener() {
+        desktop.getFirstPage().removeEventListener(EVENT_NAME, listener);
     }
 
     public void getCurrentPosition() {
         Clients.evalJavaScript("GeolocationHelper.getCurrentPosition()");
     }
 
+    /**
+     * Dispose this helper, removing the page listener and the helper JavaScript.
+     * After calling this method, this instance should not be used anymore.
+     */
     public void dispose() {
-        Desktop desktop = Executions.getCurrent().getDesktop();
         desktop.removeAttribute(GEOLOCATION_HELPER_KEY);
+        removePageListener();
+        removeHelperJavaScript();
     }
 
     protected static void ensureExecutionAvailable() {
